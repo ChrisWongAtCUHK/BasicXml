94d208caa4f73dc53633ffe7e925fe606cbfdfd9
Chris Wong <wcwong@MaximizerSoftware.onmicrosoft.com>
2017-03-01 16:19:28 +0800

    Update node.

--- XmlClassLibrary/ContactsXmlHelper.cs
+++ XmlClassLibrary/ContactsXmlHelper.cs
@@ -11,6 +11,7 @@ namespace XmlClassLibrary
     public class ContactsXmlHelper
     {
         private string filePath;
+        private XDocument doc;
 
         /// <summary>
         /// Constructor
@@ -19,6 +20,22 @@ namespace XmlClassLibrary
         public ContactsXmlHelper(string filePath)
         {
             this.filePath = filePath;
+            if (!File.Exists(filePath))
+            {
+                // if the file does not exist, create it first
+                using (XmlWriter writer = XmlWriter.Create(filePath))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Contacts");
+
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+            }
+
+            // if the file exists, add directly
+            this.doc = XDocument.Load(filePath);
+
         }
 
         /// <summary>
@@ -47,26 +64,69 @@ namespace XmlClassLibrary
 
         }
 
+        /// <summary>
+        /// Add node
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="email"></param>
         public void Add(string firstName, string lastName, string email)
         {
-            if (File.Exists(filePath))
-            {
-                // if the file exists, add directly
-                XDocument doc = XDocument.Load(filePath);
-                XElement contacts = doc.Element("Contacts");
+            XElement contacts = doc.Element("Contacts");
 
-                contacts.Add(new XElement("Contact",
-                                new XElement("FirstName", firstName),
-                                new XElement("LastName", lastName),
-                                new XElement("Email", email)
-                             )
-                );
-                doc.Save(filePath);
-            }
+            contacts.Add(new XElement("Contact",
+                            new XElement("FirstName", firstName),
+                            new XElement("LastName", lastName),
+                            new XElement("Email", email)
+                            )
+            );
+            doc.Save(filePath);
         }
 
-        // read
-        // update
+        /// <summary>
+        /// Iterate all nodes, get the elements
+        /// </summary>
+        public IEnumerable<XElement> Read()
+        {
+            XElement contacts = doc.Element("Contacts");
+
+            // linq query
+            return from contact in contacts.Descendants()
+                    select contact;
+        }
+
+        /// <summary>
+        /// Read node value by searching it with email address
+        /// </summary>
+        public XElement Read(string email)
+        {
+            XElement contacts = doc.Element("Contacts");
+
+            // linq extension
+            return contacts.Descendants()
+                .FirstOrDefault(node => node.Element("Email").Value == email);
+        }
+
+        /// <summary>
+        /// Update a node value by searching it with email address
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="email"></param>
+        public void UpdateByEmail(string firstName, string lastName, string email)
+        {
+            if (System.IO.File.Exists(filePath))
+            {
+
+                // Note: invoke Read(string email) could not update the node
+                var contact = Read(email);
+
+                contact.Element("FirstName").Value = firstName;
+                contact.Element("LastName").Value = lastName;
+                doc.Save(filePath);
+            }
+
+        }
         // delete
 
     }
--- XmlUnitTestProject/CRUDUnitTest.cs
+++ XmlUnitTestProject/CRUDUnitTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using XmlClassLibrary;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace XmlUnitTestProject
 {
@@ -16,6 +19,28 @@ namespace XmlUnitTestProject
             contactsXmlHelper.Add("Chris", "Wong", "chriswong@gmail.com");
             contactsXmlHelper.Add("Bill", "Gates", "billdates@gmail.com");
             contactsXmlHelper.Add("Steve", "Jobs", "stevejobs@gmail.com");
+        }
+
+        [TestMethod]
+        public void ReadTest()
+        {
+            IEnumerable<XElement> contactList = contactsXmlHelper.Read();
+
+            foreach (var contact in contactList)
+            {
+                var elements = contact.Elements();
+                foreach (var element in elements)
+                {
+                    Console.WriteLine(element.Name + ": " + element.Value);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            contactsXmlHelper.UpdateByEmail("Chris", "WWW", "chriswong@gmail.com");
         }
     }
 }
